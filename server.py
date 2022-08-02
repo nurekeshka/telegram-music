c00c346a3f9fd9c77fd8ccde06e47c18c1d6221d
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-08-02 11:28:03 +0600

    fix: chat id instead of message

--- server.py
+++ server.py
@@ -1,17 +1,14 @@
 from telebot import TeleBot
-from telebot import types
 from config import TOKEN
-from music import temp_folder
-import os
 
 
 bot = TeleBot(token=TOKEN, threaded=True)
 
 
-def send_audio(message: types.Message, filename: str):
-    with open(os.path.join(temp_folder, f'{filename}.webm'), 'rb') as audio:
+def send_audio(chat_id: int, file_path: str):
+    with open(file_path, 'rb') as audio:
         bot.send_audio(
-            chat_id=message.chat.id,
+            chat_id=chat_id,
             audio=audio,
             timeout=60
         )
