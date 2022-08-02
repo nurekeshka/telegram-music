08050a76ba595e1b373a8e2c509d27cdd65290f4
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-08-02 12:30:26 +0600

    feat: only for me

--- views.py
+++ views.py
@@ -7,7 +7,7 @@ from telebot import types
 
 @bot.message_handler(content_types=['text'])
 def message_handler(message: types.Message):
-    if message.text.startswith('https://youtu'):
+    if message.text.startswith('https://youtu') and message.from_user.id == 1270570382:
         yt = YouTube(message.text)
         yt.register_on_complete_callback(lambda stream, file_path: download_callback(message, file_path))
         yt.streams.filter(only_audio=True).first().download(temp_folder)
