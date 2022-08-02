9772656aecf5a9173a646120e1de1561dfe068c8
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-08-02 11:52:44 +0600

    fix: minor bug giving message instead of chat id

--- utils.py
+++ utils.py
@@ -10,5 +10,5 @@ def clear_downloads():
 
 
 def download_callback(message: types.Message, file_path: str):
-    send_audio(message, file_path)
+    send_audio(message.chat.id, file_path)
     clear_downloads()
