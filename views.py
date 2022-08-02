0c0634ade7c4ce88dde779573e1b49415f2e202d
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-08-02 11:29:43 +0600

    fix: download in views

--- views.py
+++ views.py
@@ -1,14 +1,13 @@
-from music import clear_downloads
-from music import download
-from telebot import types
-from server import send_audio
-from server import reply
+from utils import download_callback
+from constants import temp_folder
 from server import bot
+from pytube import YouTube
+from telebot import types
 
 
 @bot.message_handler(content_types=['text'])
 def message_handler(message: types.Message):
     if message.text.startswith('https://youtu'):
-        title = download(message.text)
-        send_audio(message, title)
-        clear_downloads()
+        yt = YouTube(message.text)
+        yt.register_on_complete_callback(lambda stream, file_path: download_callback(message, file_path))
+        yt.streams.filter(only_audio=True).first().download(temp_folder)
